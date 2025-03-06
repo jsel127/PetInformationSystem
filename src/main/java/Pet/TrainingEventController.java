6df1e492459835e039c2f9c72c55650f9a7329ec
Jasmine Sellers <jsel4635@gmail.com>
2025-03-06 15:59:10 -0800

    Fixed issues with loading data to table and adding a data entry for Training events.

--- src/main/java/Pet/TrainingEventController.java
+++ src/main/java/Pet/TrainingEventController.java
@@ -6,15 +6,17 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
 import javafx.scene.chart.*;
-import javafx.scene.control.Button;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
+import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.net.URL;
 import java.sql.*;
 import java.util.ResourceBundle;
@@ -49,6 +51,16 @@ public class TrainingEventController implements Initializable {
     private ComboBox myTrainingType;
     @FXML
     private LineChart<String, Integer> myLineChart;
+    @FXML
+    private Label myErrorMessage;
+    @FXML
+    private TextField myExecutionTime;
+    @FXML
+    private TextField myCommandHeldTime;
+    @FXML
+    private TextArea myNotes;
+    @FXML
+    private Button myReturnEventPageBtn;
     /***/
     @FXML
     private NumberAxis myYAxis;
@@ -102,84 +114,82 @@ public class TrainingEventController implements Initializable {
             ex.printStackTrace();
         }
     }
-
-/*    @FXML
-    public void generateErrorMessageIfTrainingTypeNull(MouseEvent theEvent) {
-        if (myTrainingType.getValue() == null) {
-            myPetMessage.setText("Pick a species first");
-        }
-    }*/
-@FXML
-public void loadTrainingEventData(ActionEvent theEvent) {
-    try {
-        Connection conn = dbConnection.getConnection();
-        myTrainingEventData = FXCollections.observableArrayList();
-
-        String query = "SELECT eventlogs.EventID, TrainingType, ExecutionTime, CommandHeldTIme, Notes\n" +
-                "FROM trainingtypes\n" +
-                "JOIN trainings ON trainings.TrainingTypeID = trainingtypes.TrainingTypeID\n" +
-                "JOIN eventlogs ON trainings.EventID = eventlogs.EventID\n" +
-                "JOIN pets ON eventlogs.PetID = pets.PetID\n" +
-                "WHERE petID = " + myPetID + "\n" +
-                "LIMIT 10;";
-        PreparedStatement pr = conn.prepareStatement(query);
-        pr.setInt(1, myEventID);
-        ResultSet rs = pr.executeQuery();
-
-        while (rs.next()) {
-            myTrainingEventData.add(new TrainingEventData(rs.getInt(1), rs.getString(2),
-                    rs.getInt(3), rs.getInt(4), rs.getString(5)));
-        }
-        conn.close();
-    } catch (SQLException ex) {
-        ex.printStackTrace();
-    }
-    myEventIDCol.setCellValueFactory(new PropertyValueFactory<TrainingEventData, Integer>("myEventID"));
-    myTrainingTypeCol.setCellValueFactory(new PropertyValueFactory<TrainingEventData, String>("myTrainingType"));
-    myExecutionTimeCol.setCellValueFactory(new PropertyValueFactory<TrainingEventData, Integer>("myExecutionTime"));
-    myCommandHeldTimeCol.setCellValueFactory(new PropertyValueFactory<TrainingEventData, Integer>("myCommandHeldTime"));
-    myNotesCol.setCellValueFactory(new PropertyValueFactory<TrainingEventData, String>("myNotes"));
-
-    myTrainingEventTable.setItems(null);
-    myTrainingEventTable.setItems(myTrainingEventData);
-}
-
-/*    @FXML
-    public void addTrainingEvent(ActionEvent theEvent) {
-        String insertEventStatement = "INSERT INTO Trainings(EventID, TrainingTypeID, ExecutionTime, CommandHeldTime, Notes) VALUES(?, ?, ?);";
+    @FXML
+    public void loadTrainingEventData(ActionEvent theEvent) {
         try {
-            if (myTrainingType.getValue() == null) {
-                //myErrorMessage.setText("A start date and time must be specified.");
-                return;
-            }
-            if (myExecutionTimeCol.getText() == "") {
-
-            }
-            if (myCommandHeldTimeCol.getText() == "") {
-                //myErrorMessage.setText("A end date and time must be specified.");
-                return;
-            }
             Connection conn = dbConnection.getConnection();
-            PreparedStatement prInsertEvent = conn.prepareStatement(insertEventStatement, Statement.RETURN_GENERATED_KEYS);
-            prInsertEvent.setInt(1, myEventID);
-            prInsertEvent.setInt(2, getMyTrainingType(myOwnershipType.getValue().toString()));
-            prInsertEvent.setInt(3, );
-            prInsertEvent.setDate(4, Date.valueOf(myDateOfAdoption.getValue()));          //Source: https://stackoverflow.com/questions/30279125/insert-date-into-mysql-database-using-javafx-datepicker
-            prInsertEvent.execute();
-            conn.close();
-            ResultSet keyData = prInsertEvent.getGeneratedKeys();
-            if (keyData.next()) {
-                myEventID = keyData.getInt(1);
-                myEventIDSelectedLabel.setText(Integer.toString(myEventID));
-                updateButtonStatuses(ENABLED);
+            myTrainingEventData = FXCollections.observableArrayList();
+
+            String query = "SELECT Trainings.EventID, TrainingType, ExecutionTime, CommandHeldTime, Notes " +
+                            "FROM Trainings JOIN TrainingTypes ON Trainings.TrainingTypeID = TrainingTypes.TrainingTypeID " +
+                            "JOIN EventLogs ON Trainings.EventID = EventLogs.EventID " +
+                            "WHERE petID = ? LIMIT 50;";
+            PreparedStatement pr = conn.prepareStatement(query);
+            pr.setInt(1, myPetID);
+            ResultSet rs = pr.executeQuery();
+
+            while (rs.next()) {
+                myTrainingEventData.add(new TrainingEventData(rs.getInt(1), rs.getString(2),
+                        rs.getInt(3), rs.getInt(4), rs.getString(5)));
             }
-            myErrorMessage.setText("");
             conn.close();
         } catch (SQLException ex) {
             ex.printStackTrace();
-            myErrorMessage.setText("Failed to create event. Check formatting of date.");
         }
-    }*/
+        myEventIDCol.setCellValueFactory(new PropertyValueFactory<TrainingEventData, Integer>("myEventID"));
+        myTrainingTypeCol.setCellValueFactory(new PropertyValueFactory<TrainingEventData, String>("myTrainingType"));
+        myExecutionTimeCol.setCellValueFactory(new PropertyValueFactory<TrainingEventData, Integer>("myExecutionTime"));
+        myCommandHeldTimeCol.setCellValueFactory(new PropertyValueFactory<TrainingEventData, Integer>("myCommandHeldTime"));
+        myNotesCol.setCellValueFactory(new PropertyValueFactory<TrainingEventData, String>("myNotes"));
+
+        myTrainingEventTable.setItems(null);
+        myTrainingEventTable.setItems(myTrainingEventData);
+    }
+
+    @FXML
+    public void addTrainingEvent(ActionEvent theEvent) {
+        String insertEventStatement = "INSERT INTO Trainings(EventID, TrainingTypeID, ExecutionTime, CommandHeldTime, Notes) VALUES(?, ?, ?, ?, ?);";
+        try {
+            if (myTrainingType.getValue() == null) {
+                myErrorMessage.setText("A training type must be specified");
+                return;
+            }
+            Connection conn = dbConnection.getConnection();
+            PreparedStatement prInsertEvent = conn.prepareStatement(insertEventStatement);
+            prInsertEvent.setInt(1, myEventID);
+            prInsertEvent.setInt(2, getMyTrainingTypeID());
+            prInsertEvent.setObject(3, Integer.parseInt(myExecutionTime.getText()));
+            prInsertEvent.setObject(4, Integer.parseInt(myCommandHeldTime.getText()));
+            prInsertEvent.setObject(5, myNotes.getText());          //Source: https://stackoverflow.com/questions/30279125/insert-date-into-mysql-database-using-javafx-datepicker
+            prInsertEvent.execute();
+            conn.close();
+            myErrorMessage.setText("Sucessfully added training event.");
+        } catch (SQLException ex) {
+            myErrorMessage.setText("Failed to create event. Only of each event type can be added for a given event.");
+        }
+    }
+
+    /**
+     * Gets the id of the potty type.
+     * @return key of the selected potty type
+     */
+    @FXML
+    private int getMyTrainingTypeID() {
+        String queryGetPottyTypeID = "SELECT TrainingTypeID FROM TrainingTypes WHERE TrainingType = ?;";
+        try {
+            Connection conn = dbConnection.getConnection();
+            PreparedStatement prPottiesType = conn.prepareStatement(queryGetPottyTypeID);
+            prPottiesType.setString(1, myTrainingType.getValue().toString());
+            ResultSet rsPottiesType = prPottiesType.executeQuery();
+            if (rsPottiesType.next()) {
+                return rsPottiesType.getInt(1);
+            }
+            conn.close();
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+        return 1;
+    }
 
     public void generateLineChart() {
         try {
@@ -207,5 +217,29 @@ public void loadTrainingEventData(ActionEvent theEvent) {
             ex.printStackTrace();
         }
     }
+    /**
+     * Returns to the pet page.
+     * @param theEvent the triggering event
+     */
+    @FXML
+    public void returnToEventPage(ActionEvent theEvent) {
+        try {
+            Stage currentStage = (Stage) myReturnEventPageBtn.getScene().getWindow();
+            currentStage.close();
 
+            Stage expenseStage = new Stage();
+            FXMLLoader loader = new FXMLLoader();
+            Pane root = (Pane) loader.load(getClass().getResource("/Pet/event.fxml").openStream());
+
+            EventController eventController = (EventController) loader.getController();
+            eventController.setPetID(myPetID);
+            Scene scene = new Scene(root);
+            expenseStage.setScene(scene);
+            expenseStage.setTitle("Event Dashboard");
+            expenseStage.setResizable(false);
+            expenseStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
