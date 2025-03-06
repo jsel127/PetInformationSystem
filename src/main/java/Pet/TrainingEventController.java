2bd60b27df6b370378618a7d301ed80c3d7fcaaa
LionCub123 <sellybeyene@outlook.com>
2025-03-06 13:30:07 -0800

    Bug in TrainingEventController.

--- src/main/java/Pet/TrainingEventController.java
+++ src/main/java/Pet/TrainingEventController.java
@@ -1,16 +1,22 @@
 package Pet;
 
 import Database.dbConnection;
+import Owner.OwnershipData;
+import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.chart.*;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.input.MouseEvent;
 
 import java.net.URL;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.ResourceBundle;
 
 /**
@@ -28,18 +34,33 @@ public class TrainingEventController implements Initializable {
     /** Stores loaded events */
     private ObservableList<TrainingEventData> myTrainingEventData;
     @FXML
+    private TableView<TrainingEventData> myTrainingEventTable;
+    @FXML
+    private TableColumn<TrainingEventData, Integer> myEventIDCol;
+    @FXML
+    private TableColumn<TrainingEventData, String> myTrainingTypeCol;
+    @FXML
+    private TableColumn<TrainingEventData, Integer> myExecutionTimeCol;
+    @FXML
+    private TableColumn<TrainingEventData, Integer> myCommandHeldTimeCol;
+    @FXML
+    private TableColumn<TrainingEventData, String> myNotesCol;
+    @FXML
+    private ComboBox myTrainingType;
+    @FXML
     private LineChart<String, Integer> myLineChart;
     /***/
     @FXML
     private NumberAxis myYAxis;
     @FXML
     private CategoryAxis myXAxis;
+    @FXML
+    private Button myLogTrainingEventBtn;
     /** Initializes the window and connects to the database */
     public void initialize(URL theURL, ResourceBundle theRB) {
-        //myXAxis.setLabel("Time of Day");
-        //myYAxis.setLabel("Command Held Time");
-        //myLineChart.setTitle("Training Report");
+        //myEventID = -1;
         myConnection = new dbConnection();
+        initializeTrainingTypeComboBox();
     }
 
     /**
@@ -63,6 +84,102 @@ public class TrainingEventController implements Initializable {
         }
         myEventID = theEventID;
     }
+
+    private void initializeTrainingTypeComboBox() {
+        String query = "SELECT TrainingType FROM TrainingTypes";
+        try {
+            Connection conn = dbConnection.getConnection();
+            PreparedStatement pr = conn.prepareStatement(query);
+            ResultSet rs = pr.executeQuery();
+            ObservableList<String> trainingTypes = FXCollections.observableArrayList();
+            while (rs.next()) {
+                trainingTypes.add(rs.getString(1));
+            }
+            myTrainingType.setItems(trainingTypes);
+            //myPetOwnershipType.setItems(ownershipTypes);
+            conn.close();
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+/*    @FXML
+    public void generateErrorMessageIfTrainingTypeNull(MouseEvent theEvent) {
+        if (myTrainingType.getValue() == null) {
+            myPetMessage.setText("Pick a species first");
+        }
+    }*/
+@FXML
+public void loadTrainingEventData(ActionEvent theEvent) {
+    try {
+        Connection conn = dbConnection.getConnection();
+        myTrainingEventData = FXCollections.observableArrayList();
+
+        String query = "SELECT eventlogs.EventID, TrainingType, ExecutionTime, CommandHeldTIme, Notes\n" +
+                "FROM trainingtypes\n" +
+                "JOIN trainings ON trainings.TrainingTypeID = trainingtypes.TrainingTypeID\n" +
+                "JOIN eventlogs ON trainings.EventID = eventlogs.EventID\n" +
+                "JOIN pets ON eventlogs.PetID = pets.PetID\n" +
+                "WHERE petID = " + myPetID + "\n" +
+                "LIMIT 10;";
+        PreparedStatement pr = conn.prepareStatement(query);
+        pr.setInt(1, myEventID);
+        ResultSet rs = pr.executeQuery();
+
+        while (rs.next()) {
+            myTrainingEventData.add(new TrainingEventData(rs.getInt(1), rs.getString(2),
+                    rs.getInt(3), rs.getInt(4), rs.getString(5)));
+        }
+        conn.close();
+    } catch (SQLException ex) {
+        ex.printStackTrace();
+    }
+    myEventIDCol.setCellValueFactory(new PropertyValueFactory<TrainingEventData, Integer>("myEventID"));
+    myTrainingTypeCol.setCellValueFactory(new PropertyValueFactory<TrainingEventData, String>("myTrainingType"));
+    myExecutionTimeCol.setCellValueFactory(new PropertyValueFactory<TrainingEventData, Integer>("myExecutionTime"));
+    myCommandHeldTimeCol.setCellValueFactory(new PropertyValueFactory<TrainingEventData, Integer>("myCommandHeldTime"));
+    myNotesCol.setCellValueFactory(new PropertyValueFactory<TrainingEventData, String>("myNotes"));
+
+    myTrainingEventTable.setItems(null);
+    myTrainingEventTable.setItems(myTrainingEventData);
+}
+
+/*    @FXML
+    public void addTrainingEvent(ActionEvent theEvent) {
+        String insertEventStatement = "INSERT INTO Trainings(EventID, TrainingTypeID, ExecutionTime, CommandHeldTime, Notes) VALUES(?, ?, ?);";
+        try {
+            if (myTrainingType.getValue() == null) {
+                //myErrorMessage.setText("A start date and time must be specified.");
+                return;
+            }
+            if (myExecutionTimeCol.getText() == "") {
+
+            }
+            if (myCommandHeldTimeCol.getText() == "") {
+                //myErrorMessage.setText("A end date and time must be specified.");
+                return;
+            }
+            Connection conn = dbConnection.getConnection();
+            PreparedStatement prInsertEvent = conn.prepareStatement(insertEventStatement, Statement.RETURN_GENERATED_KEYS);
+            prInsertEvent.setInt(1, myEventID);
+            prInsertEvent.setInt(2, getMyTrainingType(myOwnershipType.getValue().toString()));
+            prInsertEvent.setInt(3, );
+            prInsertEvent.setDate(4, Date.valueOf(myDateOfAdoption.getValue()));          //Source: https://stackoverflow.com/questions/30279125/insert-date-into-mysql-database-using-javafx-datepicker
+            prInsertEvent.execute();
+            conn.close();
+            ResultSet keyData = prInsertEvent.getGeneratedKeys();
+            if (keyData.next()) {
+                myEventID = keyData.getInt(1);
+                myEventIDSelectedLabel.setText(Integer.toString(myEventID));
+                updateButtonStatuses(ENABLED);
+            }
+            myErrorMessage.setText("");
+            conn.close();
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+            myErrorMessage.setText("Failed to create event. Check formatting of date.");
+        }
+    }*/
 
     public void generateLineChart() {
         try {
--- src/main/java/Pet/TrainingEventData.java
+++ src/main/java/Pet/TrainingEventData.java
@@ -1,4 +1,88 @@
 package Pet;
 
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+
+/**
+ * Stores individual data entries for the Event Table.
+ *
+ * @version March 5, 2025
+ * @author Jasmine Sellers
+ */
 public class TrainingEventData {
+    private final IntegerProperty myEventID;
+    private final StringProperty myTrainingType;
+    private final IntegerProperty myExecutionTime;
+    private final IntegerProperty myCommandHeldTime;
+    private final StringProperty myNotes;
+
+    public TrainingEventData(Integer theEventID, String theTrainingType, Integer theExecutionTime, Integer theCommandHeldTime, String theNotes) {
+        myEventID = new SimpleIntegerProperty(theEventID);
+        myTrainingType = new SimpleStringProperty(theTrainingType);
+        myExecutionTime = new SimpleIntegerProperty(theExecutionTime);
+        myCommandHeldTime = new SimpleIntegerProperty(theCommandHeldTime);
+        myNotes = new SimpleStringProperty(theNotes);
+    }
+
+    public int getMyEventID() {
+        return myEventID.get();
+    }
+
+    public IntegerProperty myEventIDProperty() {
+        return myEventID;
+    }
+
+    public void setMyEventID(int myEventID) {
+        this.myEventID.set(myEventID);
+    }
+
+    public String getMyTrainingType() {
+        return myTrainingType.get();
+    }
+
+    public StringProperty myTrainingTypeProperty() {
+        return myTrainingType;
+    }
+
+    public void setTrainingType(String myStartDateTime) {
+        this.myTrainingType.set(myStartDateTime);
+    }
+
+    public int getMyExecutionTime() {
+        return myExecutionTime.get();
+    }
+
+    public IntegerProperty myExecutionTime() {
+        return myExecutionTime;
+    }
+
+    public void setMyExecutionTime(int myExecutionTime) {
+        this.myExecutionTime.set(myExecutionTime);
+    }
+
+    public int getMyCommandHeldTime() {
+        return myCommandHeldTime.get();
+    }
+
+    public IntegerProperty myCommandHeldTime() {
+        return myCommandHeldTime;
+    }
+
+    public void setMyCommandHeldTime(int myCommandHeldTime) {
+        this.myCommandHeldTime.set(myCommandHeldTime);
+    }
+
+    public String getMyNotes() {
+        return myNotes.get();
+    }
+
+    public StringProperty myNotes() {
+        return myNotes;
+    }
+
+    public void setMyNotes(String myNotes) {
+        this.myNotes.set(myNotes);
+    }
 }
