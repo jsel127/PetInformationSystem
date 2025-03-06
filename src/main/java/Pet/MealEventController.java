d0fe1ee3b434576d78637ca9c5f0101789a6449d
Jasmine Sellers <jsel4635@gmail.com>
2025-03-06 03:20:42 -0800

    Implemented load Meal table.

--- src/main/java/Pet/MealEventController.java
+++ src/main/java/Pet/MealEventController.java
@@ -2,6 +2,7 @@ package Pet;
 
 import Database.dbConnection;
 import Owner.OwnerController;
+import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -9,11 +10,18 @@ import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.net.URL;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ResourceBundle;
 
 /**
@@ -32,6 +40,15 @@ public class MealEventController implements Initializable {
     private ObservableList<MealEventData> myMealEventData;
     @FXML
     private Button myReturnEventPageBtn;
+    @FXML
+    private TableView<MealEventData> myMealTable;
+    @FXML
+    private TableColumn<MealEventData, Integer> myEventIDCol;
+    @FXML
+    private TableColumn<MealEventData, String> myMealTypeCol;
+    @FXML
+    private TableColumn<MealEventData, String> myNotesCol;
+
     /** Initializes the window and connects to the database */
     public void initialize(URL theURL, ResourceBundle theRB) {
         myConnection = new dbConnection();
@@ -84,4 +101,37 @@ public class MealEventController implements Initializable {
             e.printStackTrace();
         }
     }
+    /**
+     * Loads at most 50 events from the Meal Event Table and stores it to be loaded in the UI.
+     * @param theEvent the action taken.
+     */
+    @FXML
+    public void loadMealEventData(ActionEvent theEvent) {
+        try {
+            Connection conn = dbConnection.getConnection();
+            myMealEventData = FXCollections.observableArrayList();
+
+            String query = "SELECT Meals.EventID, MealTypes.MealType, Meals.Notes " +
+                           "FROM Meals JOIN MealTypes ON Meals.MealTypeID = MealTypes.MealTypeID " +
+                           "JOIN EventLogs ON Meals.EventID = EventLogs.EventID " +
+                           "WHERE PetID = ? LIMIT 50;";
+            PreparedStatement pr = conn.prepareStatement(query);
+            pr.setInt(1, myPetID);
+            ResultSet rs = pr.executeQuery();
+
+            while (rs.next()) {
+                myMealEventData.add(new MealEventData(rs.getInt(1), rs.getString(2),
+                                    rs.getString(3)));
+            }
+            conn.close();
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+        myEventIDCol.setCellValueFactory(new PropertyValueFactory<MealEventData, Integer>("myEventID"));
+        myMealTypeCol.setCellValueFactory(new PropertyValueFactory<MealEventData, String>("myMealType"));
+        myNotesCol.setCellValueFactory(new PropertyValueFactory<MealEventData, String>("myNotes"));
+        myMealTable.setItems(null);
+        myMealTable.setItems(myMealEventData);
+    }
+
 }
